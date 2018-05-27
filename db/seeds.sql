e30d72d9f9e06dcbabfb79aa2f0af296dd59eaec
maogdamian <maogdamian@gmail.com>
2018-05-27 01:02:54 -0700

    first push of burger code

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Burgers into Database
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Cheeseburger", false, "2018-27-05 04:20:00"),
+  ("Hamburger", false, "2018-27-05 04:20:01"),
+  ("Bacon Burger", false, "2018-27-05 04:20:02"),
+  ("Bison Burger", false, "2018-27-05 16:20:00"),
+  ("Green-Chili Burger", false, "2018-27-05 16:20:01"),
+  ("Veggie Burger", false, "2018-27-05 04:20:20"),
+  ("Western Burger", true, "2018-27-05 16:20:16");
